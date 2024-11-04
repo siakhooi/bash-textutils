fb5de7aed43cbdd294ed88f8069f2909d1e8ae95
Siak Hooi <siakhooi@gmail.com>
2024-11-04 05:23:41 +0000

    initial rpm version

--- .github/bin/deploy-to-siakhooi-rpms.sh
+++ .github/bin/deploy-to-siakhooi-rpms.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-PATH_TO_FILE=$(ls ./*.deb)
-DEBIAN_PACKAGE_SOURCE_PATH=$(realpath "$PATH_TO_FILE")
-DEBIAN_PACKAGE_FILE=$(basename "$PATH_TO_FILE")
+PATH_TO_FILE=$(ls ./*.rpm)
+RPM_PACKAGE_SOURCE_PATH=$(realpath "$PATH_TO_FILE")
+RPM_PACKAGE_FILE=$(basename "$PATH_TO_FILE")
 
 TMPDIR=$(mktemp -d)
 
-readonly TARGETPATH=docs/pool/main/binary-amd64
-readonly TARGETURL=https://${PUBLISH_TO_GITHUB_REPO_TOKEN}@github.com/siakhooi/apt.git
+readonly TARGETPATH=docs
+readonly TARGETURL=https://${PUBLISH_TO_GITHUB_REPO_TOKEN}@github.com/siakhooi/rpms.git
 readonly TARGETBRANCH=main
-readonly TARGETDIR=apt
+readonly TARGETDIR=rpms
 readonly TARGET_GIT_EMAIL=textutils@siakhooi.github.io
 readonly TARGET_GIT_USERNAME=textutils
 TARGET_COMMIT_MESSAGE="bash-textutils: Auto deploy [$(date)]"
@@ -30,8 +30,8 @@ fi
   git remote set-url origin "$TARGETURL"
   git restore --staged .
   mkdir -p $TARGETPATH
-  cp -v "$DEBIAN_PACKAGE_SOURCE_PATH" "$TARGETPATH/$DEBIAN_PACKAGE_FILE"
-  git add "$TARGETPATH/$DEBIAN_PACKAGE_FILE"
+  cp -v "$RPM_PACKAGE_SOURCE_PATH" "$TARGETPATH/$RPM_PACKAGE_FILE"
+  git add $TARGETPATH/"$RPM_PACKAGE_FILE"
   git status
   git commit -m "$TARGET_COMMIT_MESSAGE"
   git push
